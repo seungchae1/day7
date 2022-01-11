e095e508c91aac2b9b1caf1f967d9b8ef8c7c394
박승채 <@DESKTOP-P53MBUO>
2022-01-11 15:23:20 +0900

    상속

---
+++ src/test/p1/Parent.java
@@ -0,0 +1,10 @@
+package test.p1;
+
+public class Parent {
+	// 상속관계일 때는 자식이 부모의 protected 멤버를 접근할 수 있다.
+	protected int num1;
+	
+	protected void test1() {
+		System.out.println("Parent의 test1() 호출");
+	}
+}
---
+++ src/test/p2/Child.java
@@ -0,0 +1,10 @@
+package test.p2;
+
+import test.p1.Parent;
+
+public class Child extends Parent {
+	public void testPRotected() {
+		num1 = 100;
+		test1();
+	}
+}
